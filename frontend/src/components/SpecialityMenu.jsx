80a427dc4d255bb3ef3f4b33f4083c3096d13898
Alex <alexchoi357@outlook.com>
2025-10-06 22:59:46 -0700

    most updated working so far 2025 10 06

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -4,13 +4,13 @@ import { Link } from 'react-router-dom'
 
 const SpecialityMenu = () => {
   return (
-    <div id='speciality' className='flex flex-col items-center gap-4 py-16 text-gray-800'>
+    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
         <h1 className='text-3xl font-medium'>Find by Speciality</h1>
         <h2 className='sm:w-1/3 text-center text-sm'>Browse through out doctors list and schedule your appointment</h2>
 
         <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
             {specialityData.map((item, index)=>(
-                <Link onClick={()=>scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={'/doctors/${items.speciality}'}>
+                <Link onClick={()=>scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${item.speciality}`}>
                     <img className="w-16 sm:w-24 mb-2" src={item.image} alt=''></img>
                     <p>{item.speciality}</p>
                 </Link>
@@ -22,3 +22,4 @@ const SpecialityMenu = () => {
 }
 
 export default SpecialityMenu
+
