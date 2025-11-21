66f4ab64ce2541693c9bb0b7eca9d45bb63c2c14
Alex <alexchoi357@outlook.com>
2025-11-21 04:09:35 -0800

    api for adding doctors is complete

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,8 @@
 import validator from "validator"
+import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from "cloudinary"
+import doctorModel from "../models/doctorModel.js"
+
 
 //api for adding doctor
 const addDoctor = async (req, res) => {
@@ -19,8 +23,42 @@ const addDoctor = async (req, res) => {
 
         }
 
-    }catch (error) {
+        // validating strong password
+        if (password.length < 8) {
+            return res.json({success:false, message:"please enter a longer/stronger password"})
+        }
 
+        //hasing doctor password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        // upload image to cloudinary
+        const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:"image",})
+        const imageUrl = imageUpload.secure_url
+
+        //doctor data
+        const doctorData = {
+            name,
+            email,
+            image:imageUrl,
+            password:hashedPassword,
+            speciality,
+            degree,
+            experience,
+            about,
+            fees,
+            address:JSON.parse(address),
+            date:Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData)
+        await newDoctor.save()
+
+        res.json({success:true, message:"doctor added successfuly"})
+
+    }catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
     }
 
 }
