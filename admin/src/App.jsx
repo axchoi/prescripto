67ffe0620846bdec6fd6af1005c48de6c71688bd
Alex <alexchoi357@outlook.com>
2025-11-29 22:10:14 -0800

    admin side development changes so far

---
+++ admin/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+import { ToastContainer, toast } from 'react-toastify';
+import { AppContext } from './context/AppContext';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './components/Navbar';
+import Sidebar from './components/sidebar';
+
+
+const App = () => {
+
+  const {aToken} = useContext(AdminContext)
+
+  return  aToken ?(
+    <div  className='bg-[#F8F9FD'>
+      <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+      </div>
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer/>
+    </>
+  )
+}
+
+export default App
--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator"
 import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
+import jwt from 'jsonwebtoken'
 
 
 //api for adding doctor
@@ -63,4 +64,22 @@ const addDoctor = async (req, res) => {
 
 }
 
-export {addDoctor};
+// CREAT API FOR ADMIN LOGIN
+const loginAdmin = async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password, process.env.JWT_SECRET) //this gives you a token
+            res.json({success:true, token})
+        } else {
+            res.json({success:false, message:'Invalid token/email or password'})
+        }
+        
+    } catch (error) {
+        console.log(error)
+        res.json({success:false, message:error.message})
+    }
+}
+
+export {addDoctor, loginAdmin};
